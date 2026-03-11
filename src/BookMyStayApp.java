c111d187adfacfdaf0f08695caa9fe41af86e30a
Manaswini-2006 <sb6468@srmist.edu.in>
2026-03-11 15:16:45 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,10 +1,65 @@
-public class BookMyStayApp {
 
+abstract class Room {
 
-    public static void main(String[] args) {
+    private String roomType;
+    private int beds;
+    private int size;
+    private double price;
 
-        System.out.println("        Welcome to BookMyStay App      ");
-        System.out.println("      Hotel Booking Management System  ");
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price: " + price);
     }
 }
 
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 200, 80.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 120.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 250.0);
+    }
+}
+
+public class BookMyStayApp{
+
+    public static void main(String[] args) {
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
+}
