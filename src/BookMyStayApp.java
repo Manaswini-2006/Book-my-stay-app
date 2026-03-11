1d7cce1faffe08b3b3ddcfbc937ef60020e0a500
Manaswini-2006 <sb6468@srmist.edu.in>
2026-03-11 15:06:08 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,4 +1,10 @@
 public class BookMyStayApp {
+
+
     public static void main(String[] args) {
+
+        System.out.println("        Welcome to BookMyStay App      ");
+        System.out.println("      Hotel Booking Management System  ");
     }
 }
+
